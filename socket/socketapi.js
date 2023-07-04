05b1ea263bd28038b40ef097a4b5323042661c3b
Afeef <afeefpk80@gmail.com>
2023-07-04 18:47:50 +0530

    updated package

--- socket/socketapi.js
+++ socket/socketapi.js
@@ -6,17 +6,13 @@ const socketapi = {
 };
 
 io.on('connection',(socket)=>{
-    console.log("A user Connected");
-
     //join goup
     socket.on('joinGroup', groupId => {
-        console.log(`Client joined group ${groupId}`);
         socket.join(groupId);
     });
 
     //send message
     socket.on('sendMessage',async({userId,groupId,text})=>{
-        console.log('send Message');
         let sender = await User.findOne({ _id: userId }, { name: 1, image:1});
         io.to(groupId).emit('receiveMessage', { sender, groupId, text });
     })
@@ -24,14 +20,12 @@ io.on('connection',(socket)=>{
 
     //send image
     socket.on("sendFile", async ({groupId,sender,type,file,text}) => {
-        console.log("sendImage");
         let user = await User.findOne({ _id: sender }, { name: 1, image:1 });
         io.to(groupId).emit('receiveMessage', { sender:user, text ,type,file,groupId});
     });
 
     // Clean up when the client disconnects
     socket.on('disconnect', () => {
-        console.log('Client disconnected');
     });
 
 })
